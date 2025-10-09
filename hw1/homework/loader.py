807625160802e38e6c72bcc54b76d2c09912c458
Polina Maximenko <98345031+polinamaximenko@users.noreply.github.com>
2025-10-09 17:20:49 +0300

    add hw1
    add hw1/homework

---
+++ hw1/homework/loader.py
@@ -0,0 +1,40 @@
+# Импорт класса
+from loader_script import DataLoader
+
+def dataloader_example(file_path):
+    """Проверяет работу методов класса DataLoader()"""
+
+    # 1. Создание экземпляра
+    loader = DataLoader()
+
+    # 2. Загрузка данных
+    try:
+        print(f"Загрузка данных из файла '{file_path}'...")
+        data = loader.load_data(file_path)
+        print("Данные успешно загружены!\n")
+    except FileNotFoundError as e:
+        print(f"Ошибка: {e}")
+    except Exception as e:
+        print(f"Ошибка загрузки: {e}")
+
+    # 3. Анализ данных
+    try:
+        info = loader.get_basic_info()
+        print(info)
+    except ValueError as e:
+        print(f"Ошибка анализа: {e}")
+
+    # 4. Работа с данными напрямую
+    if loader.data is not None:
+        print("\nПервые 5 строк данных:")
+        print(loader.data.head())
+
+# Вызов функции dataloader_example для 3 файлов-примеров
+print("** Пример 1 **\n")
+dataloader_example('health_metrics.csv')
+
+print("\n** Пример 2 **\n")
+dataloader_example('sales_data.csv')
+
+print("\n** Пример 3 **\n")
+dataloader_example('user_behavior.csv')
---
+++ hw1/homework/loader_script.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import os
+
+class DataLoader():
+    def __init__(self):
+        self.data = None
+
+    def _validate_file_path(self, file_path : str = 'data.csv'):
+        """Проверяет существование пути файла и то, что это файл (а не директория)"""
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"Файл '{file_path}' не существует")
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError(f"'{file_path}' является директорией, а не файлом")
+
+    def load_data(self, file_path : str = 'data.csv'):
+        """Загружает данные и возвращает в формате DataFrame"""
+        self._validate_file_path(file_path)
+        self.data = pd.read_csv(file_path)
+        return self.data
+
+    def get_basic_info(self):
+        """Выводит базовую информацию о датасете: форму (shape), имена колонок и количество пропущенных значений в каждой колонке"""
+        if self.data is not None:
+            shape = self.data.shape
+            columns_lst = self.data.columns.to_list()
+            nulls_lst = [int(self.data[c].isnull().sum()) for c in columns_lst]
+            return f"Форма датасета: {shape}\nНазвания колонок: {columns_lst}\nКоличество пропущенных значений по колонкам: {nulls_lst}"
+        else:
+            raise ValueError("Данные не загружены")
